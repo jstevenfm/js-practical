d7147542f68481b97c547ecac2744e8e18788c54
jstevenfm <jstevenfm@gmail.com>
2021-09-15 19:04:14 -0500

    Agregando funciones para aplicar cupones de descuento.

--- preciosDescuentos/descuentos.js
+++ preciosDescuentos/descuentos.js
@@ -1,21 +1,39 @@
 // const priceOrigin = 120;
 // const desct = 18;
 
-function calcularPrecioConDescuento(precio, descuento){
+function calcularPrecioConDescuento(precio, descuento) {
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
 
     return precioConDescuento;
 }
 
+const coupons = [
+    "Empecemos_Suave15",
+    "Mucho_Mas30",
+    "Vamos_Por_Mas25",
+];
+
 function buttonPriceDiscount() {
     const inputPrice = document.getElementById("inputPrice");
     const priceValue = inputPrice.value;
 
-    const inputDiscount = document.getElementById("inputDiscount");
-    const discountValue = inputDiscount.value;
+    const inputCoupon = document.getElementById("inputCoupon");
+    const couponValue = inputCoupon.value;
 
-    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+    let descuento;
+
+    if (!coupons.includes(couponValue)) {
+        alert("El cupón " + couponValue + "no es válido");
+    } else if (couponValue === "Empecemos_Suave15") {
+        descuento = 15;
+    } else if (couponValue === "Mucho_Mas30") {
+        descuento = 30;
+    } else if (couponValue === "Vamos_Por_Mas25") {
+        descuento = 25;
+    }
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
 
     const resultPrice = document.getElementById("resultPrice");
     resultPrice.innerText = "El precio con descuento a pagar es: $" + precioConDescuento;
