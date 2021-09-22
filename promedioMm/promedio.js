edff209f7139ccd021636b2743ddff6ad73a4b90
jstevenfm <jstevenfm@gmail.com>
2021-09-21 19:23:20 -0500

    Agregué operaciones para calcular la mediana.

--- promedioMm/promedio.js
+++ promedioMm/promedio.js
@@ -1,4 +1,6 @@
-
+// 
+// MEDIA ARITMÉTICA
+// 
 function calcularPromedio(lista) {
     // let sumaLista = 0;
     // for (let i = 0; i < lista.length; i++) {
@@ -14,4 +16,39 @@ function calcularPromedio(lista) {
     const promedioLista = sumaLista / lista.length;
 
     return promedioLista;
+}
+
+// 
+// MEDIANA
+// 
+const lista1 = [
+    100,
+    300,
+    500,
+    8000,
+];
+
+const mitadLista1 = parseInt(lista1.length / 2);
+
+function par(numero) {
+    if (numero % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let mediana;
+if (par(lista1.length)) {
+    const elemento1 = lista1[mitadLista1 - 1];
+    const elemento2 = lista1[mitadLista1];
+
+    const promedioMediana = calcularPromedio([
+        elemento1,
+        elemento2,
+    ]);
+
+    mediana = promedioMediana;
+} else {
+    mediana = lista1[mitadLista1];
 }
