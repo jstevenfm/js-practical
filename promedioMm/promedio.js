90282b94396ac630bfbabf2c5e1744accb88f8d4
jstevenfm <jstevenfm@gmail.com>
2021-11-29 19:25:25 -0500

    Promedio, Mediana y Moda

--- promedioMm/promedio.js
+++ promedioMm/promedio.js
@@ -22,11 +22,12 @@ function calcularPromedio(lista) {
 // MEDIANA
 // 
 const lista1 = [
-    100,
-    300,
     500,
     8000,
+    100,
+    300,
 ];
+lista1.sort((a, b) => a - b);
 
 const mitadLista1 = parseInt(lista1.length / 2);
 
@@ -52,3 +53,44 @@ if (par(lista1.length)) {
 } else {
     mediana = lista1[mitadLista1];
 }
+
+//
+// MODA
+//
+
+const listaModa = [
+    1,
+    2,
+    3,
+    1,
+    2,
+    3,
+    4,
+    2,
+    2,
+    2,
+    2,
+    2,
+    1,
+];
+
+const listaModaCount = {};
+
+listaModa.map(
+    function (elemento) {
+        if (listaModaCount[elemento]) {
+            listaModaCount[elemento] = listaModaCount[elemento] += 1;
+        } else {
+            listaModaCount[elemento] = 1;
+        }
+
+    }
+);
+
+const listaModaArray = Object.entries(listaModaCount).sort(
+    function (valorAcumuladoModa, nuevoValor) {
+        return valorAcumuladoModa[1] - nuevoValor[1];
+    }
+);
+
+const moda = listaModaArray[listaModaArray.length - 1];
